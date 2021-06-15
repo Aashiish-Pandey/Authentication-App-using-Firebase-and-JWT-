20dc8cce0c96f3e7905ac60072704b46acc34036
Aashiish-Pandey <ash006pandey@gmail.com>
2021-06-16 01:59:19 +0530

    Redirecting the user by using useHistory Hook

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,5 +1,6 @@
 import { useState, useRef ,useContext} from "react";
 import AuthContext from "../../store/auth-context";
+import {useHistory} from 'react-router-dom'
 
 import classes from "./AuthForm.module.css";
 
@@ -9,6 +10,7 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const authCtx=useContext(AuthContext)
+  const history = useHistory()
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -56,6 +58,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken)
+        history.replace('/')
       })
       .catch((err) => {
         alert(err.message);
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,39 +1,45 @@
 import classes from "./ProfileForm.module.css";
-import { useRef,useContext } from "react";
+import { useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
-
+import { useHistory } from "react-router-dom";
 const ProfileForm = () => {
-
-   const authCtx = useContext(AuthContext)
-
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
   const newPasswordInputRef = useRef();
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const enteredNewPassword = newPasswordInputRef.current.value
+    const enteredNewPassword = newPasswordInputRef.current.value;
 
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBuMvzndMCLl88fz3-e_sPeWqPfw1Kyyzk',{
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBuMvzndMCLl88fz3-e_sPeWqPfw1Kyyzk",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
 
-    method:'POST',
-    body: JSON.stringify({
-      idToken:authCtx.token,
-      password:enteredNewPassword,
-      returnSecureToken:false,
-    }),
-    headers: {
-
-      'Content-Type': 'application/json'
-    }
-    }).then(res=>{
-      
-    })
+      history.replace('/')
+    });
   };
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" minLength="7"  ref ={newPasswordInputRef}/>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
